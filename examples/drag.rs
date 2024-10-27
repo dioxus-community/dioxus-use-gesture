df24581f926290d3854921de171cb8bf2c4b5f5e
Matt Hunzinger <matthunz2@gmail.com>
2024-10-27 17:47:22 -0400

    Publish v0.2.0-alpha.1

--- examples/drag.rs
+++ examples/drag.rs
@@ -1,27 +1,27 @@
 use dioxus::prelude::*;
-use dioxus_signals::use_signal;
 use dioxus_spring::{use_animated, use_spring_signal};
 use dioxus_use_gesture::{use_drag, DragState};
+use dioxus_use_mounted::use_mounted;
 use std::time::Duration;
 
-fn app(cx: Scope) -> Element {
-    let element_ref = use_signal(cx, || None);
+fn app() -> Element {
+    let element_ref = use_mounted();
 
-    let (spring_ref, value_ref) = use_spring_signal(cx, [0f32, 0f32]);
-    use_animated(cx, element_ref, value_ref, |[x, y]| {
+    let (value_ref, spring_ref) = use_spring_signal([0f32, 0f32]);
+    use_animated(element_ref, value_ref, |[x, y]| {
         format!("width: 200px; height: 200px; background: red; transform: translate({x}px, {y}px);")
     });
 
-    use_drag(cx, element_ref, move |state, x, y| match state {
+    use_drag(element_ref, move |state, x, y| match state {
         DragState::Move => spring_ref.set([x, y]),
         DragState::End => spring_ref.animate([0., 0.], Duration::from_millis(500)),
     });
 
-    render!(div {
-        onmounted: move |event| element_ref.set(Some(event.data.clone()))
+    rsx!(div {
+        onmounted: move |event| element_ref.onmounted(event)
     })
 }
 
 fn main() {
-    dioxus_web::launch(app)
+    dioxus::launch(app)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use dioxus::prelude::*;
-use dioxus_signals::{use_signal, Signal};
-use std::rc::Rc;
+use dioxus::{prelude::*, web::WebEventExt};
+use dioxus_use_mounted::UseMounted;
+use std::{cell::RefCell, rc::Rc};
 use wasm_bindgen::{prelude::Closure, JsCast};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -17,77 +17,65 @@ struct State {
     start: Option<(f32, f32)>,
 }
 
-pub fn use_drag<T>(
-    cx: Scope<T>,
-    element_ref: Signal<Option<Rc<MountedData>>>,
-    on_drag: impl FnMut(DragState, f32, f32) + 'static,
-) {
-    let state_ref = use_signal(cx, || State::default());
+pub fn use_drag(element_ref: UseMounted, on_drag: impl FnMut(DragState, f32, f32) + 'static) {
+    let mut state_ref = use_signal(State::default);
     let handler_cell = Rc::new(RefCell::new(on_drag));
 
-    use_effect(cx, &element_ref.read().is_some(), move |_| async move {
-        let mut state = state_ref.write();
-        if let Some(mounted) = element_ref.read().clone() {
-            let element = mounted
-                .get_raw_element()
-                .unwrap()
-                .downcast_ref::<web_sys::Element>()
-                .unwrap();
+    use_future(move || {
+        let handler_cell = handler_cell.clone();
+        async move {
+            if let Some(mounted) = element_ref.signal.read().clone() {
+                let element = mounted.as_web_event();
 
-            let handler_cell_clone = handler_cell.clone();
-            state.on_pointer_move = Some(Closure::new(move |event: web_sys::PointerEvent| {
-                if let Some((start_x, start_y)) = state_ref.read().start {
-                    handler_cell_clone.borrow_mut()(
-                        DragState::Move,
-                        event.client_x() as f32 - start_x,
-                        event.client_y() as f32 - start_y,
-                    );
-                }
-            }));
-            add_listener(element, "pointermove", &state.on_pointer_move);
+                let handler_cell_clone = handler_cell.clone();
+                state_ref.write().on_pointer_move =
+                    Some(Closure::new(move |event: web_sys::PointerEvent| {
+                        if let Some((start_x, start_y)) = state_ref.read().start {
+                            handler_cell_clone.borrow_mut()(
+                                DragState::Move,
+                                event.client_x() as f32 - start_x,
+                                event.client_y() as f32 - start_y,
+                            );
+                        }
+                    }));
+                add_listener(&element, "pointermove", &state_ref.read().on_pointer_move);
 
-            let callback_mounted = mounted.clone();
-            state.on_pointer_down = Some(Closure::new(move |event: web_sys::PointerEvent| {
-                let element = callback_mounted
-                    .get_raw_element()
-                    .unwrap()
-                    .downcast_ref::<web_sys::Element>()
-                    .unwrap();
+                let callback_mounted = mounted.clone();
+                state_ref.write().on_pointer_down =
+                    Some(Closure::new(move |event: web_sys::PointerEvent| {
+                        let element = callback_mounted.as_web_event();
 
-                let rect = element.get_bounding_client_rect();
+                        let rect = element.get_bounding_client_rect();
 
-                state_ref.write().start = Some((
-                    event.client_x() as f32 - rect.left() as f32,
-                    event.client_y() as f32 - rect.top() as f32,
-                ));
-            }));
-            add_listener(element, "pointerdown", &state.on_pointer_down);
+                        state_ref.write().start = Some((
+                            event.client_x() as f32 - rect.left() as f32,
+                            event.client_y() as f32 - rect.top() as f32,
+                        ));
+                    }));
+                add_listener(&element, "pointerdown", &state_ref.read().on_pointer_down);
 
-            state.on_pointer_up = Some(Closure::new(move |event: web_sys::PointerEvent| {
-                handler_cell.borrow_mut()(
-                    DragState::End,
-                    event.client_x() as _,
-                    event.client_y() as _,
-                );
-                state_ref.write().start.take();
-            }));
-            add_listener(element, "pointerup", &state.on_pointer_up);
+                state_ref.write().on_pointer_up =
+                    Some(Closure::new(move |event: web_sys::PointerEvent| {
+                        handler_cell.borrow_mut()(
+                            DragState::End,
+                            event.client_x() as _,
+                            event.client_y() as _,
+                        );
+                        state_ref.write().start.take();
+                    }));
+                add_listener(&element, "pointerup", &state_ref.read().on_pointer_up);
+            }
         }
     });
 
-    let state_ref_clone = state_ref.clone();
-    use_on_destroy(cx, move || {
-        let mut state = state_ref_clone.write();
-        if let Some(mounted) = element_ref.read().clone() {
-            let element = mounted
-                .get_raw_element()
-                .unwrap()
-                .downcast_ref::<web_sys::Element>()
-                .unwrap();
+    use_drop(move || {
+        let mut state = state_ref.write();
+        if let Some(mounted) = element_ref.signal.read().clone() {
+            let element = mounted.as_web_event();
 
-            remove_listener(element, "pointerdown", &mut state.on_pointer_down);
-            remove_listener(element, "pointermove", &mut state.on_pointer_move);
-            remove_listener(element, "pointerup", &mut state.on_pointer_up);
+            remove_listener(&element, "pointerdown", &mut state.on_pointer_down);
+            remove_listener(&element, "pointermove", &mut state.on_pointer_move);
+            remove_listener(&element, "pointerup", &mut state.on_pointer_up);
         }
     });
 }
